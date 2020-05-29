7eaff4402a6080e1f0f22043e97ead14a04048ba
Advait Joshi <advaitjoshi1998@gmail.com>
2020-05-29 17:19:12 +0300

    [name_search] fix paths and writing logic

--- name_search.py
+++ name_search.py
@@ -3,11 +3,11 @@ import xlsxwriter
 
 # USERS_LOC = location of usernames_file
 # USERS_COL = column of usernames in file
-USERS_LOC = r"C:\Users\PAVILION\Desktop\usernames.xlsx"
+USERS_LOC = r"C:\Users\PAVILION\Documents\repos\excel_repo\files\usernames.xlsx"
 USERS_COL = 2
 # HR_LOC = location of HR_List file
 # HR_COL = Column of usernames in file
-HR_LOC = r"C:\Users\PAVILION\Desktop\HR_List.xlsx"
+HR_LOC = r"C:\Users\PAVILION\Documents\repos\excel_repo\files\HR_List.xlsx"
 HR_COL = 2
 
 
@@ -15,7 +15,7 @@ HR_COL = 2
 # or the new results will be appended to the bottom
 # of the generated file
 # RESULTS_PATH = intended path for results file
-RESULTS_PATH = r"C:\Users\PAVILION\Documents\repos\excel_scripts\results\name_search.xlsx"
+RESULTS_PATH = r"C:\Users\PAVILION\Documents\repos\excel_repo\results\name_search.xlsx"
 
 users_wb = xlrd.open_workbook(USERS_LOC)
 users_sheet = users_wb.sheet_by_index(0)
@@ -31,18 +31,19 @@ results_sheet = results_book.add_worksheet()
 row_count = 2
 write_row = 0
 for row in range(0, hr_sheet.nrows):
+    row_count += 1
     hr_name = hr_sheet.cell_value(row, HR_COL-1) or None
     if not hr_name:
         continue
-    names_l = []
-    rows_l = []
+    results_sheet.write(row_count, 0, hr_name)
     for name in hr_name.split(' '):
-        results_sheet.write(row_count, 0, name)
-        row_count += 1
         if len(name) < 3:
             continue
         for row in range(0, users_sheet.nrows):
             users_name = users_sheet.cell_value(row, USERS_COL-1)
             if name.lower() in users_name.lower():
+                # print(name)
                 results_sheet.write(row_count, 1, users_name)
                 row_count += 1
+
+results_book.close()
