3fabcc93281eb3ce98544e12ca0314becc37fe6d
Advait Joshi <advaitjoshi1998@gmail.com>
2020-05-31 12:53:56 +0300

    [non_digits_check] Output cleaned_data + include cleaned_data info in log

--- non_digits_check.py
+++ non_digits_check.py
@@ -20,6 +20,7 @@ today_day = today_date.strftime("%H-%M-%S.%d-%B-%Y")
 folder_path = os.path.join(DESKTOP_PATH, today_day)
 os.mkdirs(folder_path)
 ALNUM_FILE = os.path.join(folder_path, 'alnum.xlsx')
+CLEANED_FILE = os.path.join(folder_path, 'digits.xlsx')
 NULL_FILE = os.path.join(folder_path, 'null.xlsx')
 LOG_FILE = os.path.join(folder_path, 'log.txt')
 
@@ -30,6 +31,8 @@ def output_log():
                f"Input file: {LOC}\n"
                f"Rows where {COL_NAME} is alphanumeric : {ALNUM_FILE}\n"
                f"NB: if {ALNUM_FILE} does not exist, no alphanumeric values were found\n"
+               f"Rows where {COL_NAME} is a digit : {CLEANED_FILE}\n"
+               f"NB: if {CLEANED_FILE} does not exist, no digits were found\n"
                f"Rows where {COL_NAME} is NULL: {NULL_FILE}\n"
                f"NB: if {NULL_FILE} does not exist, no empty values were found."
                )
@@ -37,7 +40,7 @@ def output_log():
         f.write(log_str)
 
 
-def alnum_check(df):
+def alnum_check(df, flagged_file):
     """Flag non-decimal values in COL_NAME.
 
     Output rows with non-decimal value in COL_NAME to ALNUM_FILE.
@@ -45,12 +48,16 @@ def alnum_check(df):
     df["Check"] = df[COL_NAME].astype(str).str.isdecimal()
     flagged_df = df[df["Check"] == False]
     flagged_df = flagged_df.drop(["Check"], axis=1)
-    if not flagged_df.empty:
-        flagged_df.to_excel(ALNUM_FILE)
+    output_df(flagged_df, flagged_file)
+
+    cleaned_df = df[df["Check"] == True]
+    cleaned_df = cleaned_df.drop(["Check"], axis=1)
+    return cleaned_df
 
 
 if __name__ == "__main__":
     df = pd.read_excel(LOC, header=HEADER_ROW-1)
     none_check(df, COL_NAME, NULL_FILE)
-    alnum_check(df)
+    cleaned_df = alnum_check(df, ALNUM_FILE)
+    output_df(cleaned_df, CLEANED_FILE)
     output_log()
