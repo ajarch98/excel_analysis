9da39c56af532118ae2a18c65cf8617e8e635b8e
Advait Joshi <advaitjoshi1998@gmail.com>
2020-05-31 12:48:46 +0300

    [excel_core] implement output_df in excel_core

--- excel_core.py
+++ excel_core.py
@@ -4,10 +4,9 @@ def duplicates_check(df, col_name, duplicates_file):
     Ouput rows with duplicate values to duplicates_file.
     Return dataframe with duplicates removed.
     """
-    duplicates = df[df.duplicated(subset=col_name, keep=False)]
-    duplicates = duplicates.sort_values(by=col_name)
-    if not duplicates.empty:
-        duplicates.to_excel(duplicates_file)
+    duplicates_df = df[df.duplicated(subset=col_name, keep=False)]
+    duplicates_df = duplicates_df.sort_values(by=col_name)
+    output_df(duplicates_df, duplicates_file)
 
     non_duplicates_df = df.drop_duplicates(subset=col_name, keep=False)
     return non_duplicates_df
@@ -20,8 +19,7 @@ def none_check(df, col_name, null_file):
     """
     df[col_name] = df[col_name].map(lambda x: x.strip() if isinstance(x, str) else x)
     none_df = df[(df[col_name].isna()) | (df[col_name] == "")]
-    if not none_df.empty:
-        none_df.to_excel(null_file)
+    output_df(none_df, null_file)
     cleaned_df = df[(df[col_name].astype(str).str.len() > 0) & (df[col_name].notna())]
     return cleaned_df
 
